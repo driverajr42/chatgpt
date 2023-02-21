a7c80533506f3b2c60b26a16bb362a33b7532384
Dennis <106558555+driverajr42@users.noreply.github.com>
2023-02-20 22:00:56 -0500

    first commit of project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const HomePage = () => {
+  return (
+    <div> <h1 className='text-4xl text-center my-6'>Main Page of App</h1></div>
+  )
+}
+
+
+export default HomePage
