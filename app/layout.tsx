4b52a59a4e3291b5ce105c47386ebcc64ecf2e0b
Dennis <106558555+driverajr42@users.noreply.github.com>
2023-02-21 06:31:58 -0500

    Began to code the index page.tsx with content for the main editor section of the app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,14 @@ export default function RootLayout({
     <html>
       <head />
       <body>
-        <div>
+        <div className='flex'>
         {/* Sidebar */}
-        {children}
+
+        {/* Client Provider - Notification */}
+
+       <div className='bg-[#343541] flex-1'>
+       {children}
+       </div>
         </div>
         </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,22 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div> <h1 className='text-4xl text-center my-6'>Main Page of App</h1></div>
+    <div className='flex flex-col items-center justify-center h-screen px-2 text-white'> <h1 className='text-5xl font-bold mb-20'>Chatgpt</h1>
+    {/* Main GPT editor Container */}
+    <div>
+        <div>
+            <div className='flex flex-col items-center justify-center mb-5'>
+                {/* Sun Icon */}
+                <h2>Examples</h2>
+            </div>
+            <div className='space-y-2'>
+                <p className='infoText'>"Explain Something to me"</p>
+                <p className='infoText'>"What is the difference between a dog and a cat?"</p>
+                <p className='infoText'> "What is the color of the sun?"</p>
+            </div>
+        </div>
+    </div>
+    </div>
   )
 }
 
