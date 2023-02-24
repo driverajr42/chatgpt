bf55e71c276c708df10aabbf183a6ac7aaca58c9
Dennis <106558555+driverajr42@users.noreply.github.com>
2023-02-23 22:22:26 -0500

    Added NewChat component inside of the Sidebar component.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,23 @@
+import NewChat from './NewChat'
+
+
 export const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          {/* New Chat */}
+          <NewChat />
+
+          <div>
+            {/* Model Selection */}
+          </div>
+
+          {/* Map Through Chat Row */}
+
+        </div>
+      </div>
+
+    </div>
   )
 }
